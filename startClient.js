b4ebc40a12af98aad8c3a7ed64127a0f598e3e17
穆客 <zhangzifa.zzf@alibaba-inc.com>
2015-08-19 10:38:57 +0800

    sync with alisdk agentx when daemon agent quit

--- startClient.js
+++ startClient.js
@@ -2,47 +2,37 @@
 
 'use strict';
 
+var path = require('path');
 var cfork = require('cfork');
 var util = require('util');
+var clientPath = path.join(__dirname, 'client.js');
 
-var printUsage = function () {
-  console.log('请指定配置文件, 用法:');
-  console.log('  agentx <config.json>');
-};
-
-var readConfig = function () {
+var getConfigPath = function () {
   var argv = process.argv.slice(2);
   if (argv.length < 1) {
-    printUsage();
+    console.log('请指定配置文件, 用法:');
+    console.log('  agentx <config.json>');
     process.exit(1);
   }
-  return argv[0];
+  return path.resolve(argv[0]);
 };
 
-
 cfork({
-  exec: './client.js',
-  args: [readConfig()],
+  exec: clientPath,
+  args: [getConfigPath()],
   count: 1
 })
-.on('fork', function (worker) {
-  console.warn('[%s] [worker:%d] new worker start', Date(), worker.process.pid);
+.on('fork', function (client) {
+  console.log('[%s] [client:%d] new client start', Date(), client.process.pid);
 })
-.on('disconnect', function (worker) {
-  console.warn('[%s] [master:%s] wroker:%s disconnect, suicide: %s, state: %s.',
-    Date(), process.pid, worker.process.pid, worker.suicide, worker.state);
+.on('disconnect', function (client) {
+  console.error('[%s] [daemon:%s] client:%s disconnect, suicide: %s, state: %s.',
+    Date(), process.pid, client.process.pid, client.suicide, client.state);
 })
-.on('exit', function (worker, code, signal) {
-  var exitCode = worker.process.exitCode;
-  var err = new Error(util.format('worker %s died (code: %s, signal: %s, suicide: %s, state: %s)',
-    worker.process.pid, exitCode, signal, worker.suicide, worker.state));
-  err.name = 'WorkerDiedError';
-  console.error('[%s] [master:%s] wroker exit: %s', Date(), process.pid, err.stack);
-})
-
-// if you do not listen to this event
-// cfork will output this message to stderr
-.on('unexpectedExit', function (worker, code, signal) {
-  // logger what you want
+.on('exit', function (client, code, signal) {
+  var exitCode = client.process.exitCode;
+  var err = new Error(util.format('client %s died (code: %s, signal: %s, suicide: %s, state: %s)',
+    client.process.pid, exitCode, signal, client.suicide, client.state));
+  err.name = 'ClientDiedError';
+  console.error('[%s] [daemon:%s] client exit: %s', Date(), process.pid, err.stack);
 });
-
